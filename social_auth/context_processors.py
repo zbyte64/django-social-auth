b33aef7c6cecf00447888291b73d99a02967b7be
Jason Kraus <jasonk@cukerinteractive.com>
2011-10-27 15:24:08 -0700

    make context processors lazy

--- social_auth/context_processors.py
+++ social_auth/context_processors.py
@@ -1,7 +1,8 @@
+from django.utils.functional import SimpleLazyObject
+
 from social_auth.backends import BACKENDS
 from social_auth.utils import group_backend_by_type
 from social_auth.models import User
-
 
 # Note: social_auth_backends and social_auth_by_type_backends don't play nice
 #       together
@@ -10,7 +11,10 @@ def social_auth_backends(request):
     """Load Social Auth current user data to context.
     Will add a output from backends_data to context under social_auth key.
     """
-    return {'social_auth': backends_data(request.user)}
+    def get_backends():
+        return backends_data(request.user)
+    
+    return {'social_auth': SimpleLazyObject(get_backends)}
 
 
 def social_auth_by_type_backends(request):
@@ -18,12 +22,15 @@ def social_auth_by_type_backends(request):
     Will add a output from backends_data to context under social_auth key where
     each entry will be grouped by backend type (openid, oauth, oauth2).
     """
-    data = backends_data(request.user)
-    data['backends'] = group_backend_by_type(data['backends'])
-    data['not_associated'] = group_backend_by_type(data['not_associated'])
-    data['associated'] = group_backend_by_type(data['associated'],
-                                               key=lambda assoc: assoc.provider)
-    return {'social_auth': data}
+    def get_backends():
+        data = backends_data(request.user)
+        data['backends'] = group_backend_by_type(data['backends'])
+        data['not_associated'] = group_backend_by_type(data['not_associated'])
+        data['associated'] = group_backend_by_type(data['associated'],
+                                                   key=lambda assoc: assoc.provider)
+        return data
+    
+    return {'social_auth': SimpleLazyObject(get_backends)}
 
 
 def social_auth_by_name_backends(request):
@@ -34,14 +41,16 @@ def social_auth_by_name_backends(request):
     with a hyphen have the hyphen replaced with an underscore, e.g. 
     google-oauth2 becomes google_oauth2 when referenced in templates.
     """
-    keys = BACKENDS.keys()
-    accounts = dict(zip(keys, [None] * len(keys)))
+    def get_backends():
+        keys = BACKENDS.keys()
+        accounts = dict(zip(keys, [None] * len(keys)))
+        
+        if isinstance(request.user, User) and request.user.is_authenticated():
+            for associated in request.user.social_auth.all():
+                accounts[associated.provider.replace('-', '_')] = associated
+        return accounts
     
-    if isinstance(request.user, User) and request.user.is_authenticated():
-        for associated in request.user.social_auth.all():
-            accounts[associated.provider.replace('-', '_')] = associated
-    
-    return {'social_auth': accounts}
+    return {'social_auth': SimpleLazyObject(get_backends)}
 
 
 def backends_data(user):
